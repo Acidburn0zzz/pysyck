f08bdfdee9c3fb5755e24ef2b368aaec5fe7a50b
Kirill Simonov <xi@resolvent.net>
2005-12-27 18:44:23 +0000

    Add test for bug #33. Add test for the bug with emitting extra delimiters.

--- sandbox/emit-it/extra-delimiter-bug.c
+++ sandbox/emit-it/extra-delimiter-bug.c
@@ -4,8 +4,8 @@
 
 #include <syck.h>
 
-#define VALUE1 "this scalar contains trailing spaces  "
-#define VALUE2 "this scalar contains a trailing colon:"
+#define VALUE "a scalar"
+#define LENGTH 100000
 
 void output_handler(SyckEmitter *e, char *str, long len)
 {
@@ -14,18 +14,18 @@ void output_handler(SyckEmitter *e, char *str, long len)
 
 void emitter_handler(SyckEmitter *e, st_data_t id)
 {
+    int k;
+
     switch (id) {
         case 1:
             syck_emit_seq(e, NULL, seq_none);
-            syck_emit_item(e, 2);
-            syck_emit_item(e, 3);
-            syck_emit_end(e);
+            for (k = 2; k < LENGTH; k++) {
+                syck_emit_item(e, k);
+            }
             break;
-        case 2:
-            syck_emit_scalar(e, "tag:yaml.org,2002:str", scalar_none, 0, 0, 0, VALUE1, strlen(VALUE1));
-            break;
-        case 3:
-            syck_emit_scalar(e, "tag:yaml.org,2002:str", scalar_none, 0, 0, 0, VALUE2, strlen(VALUE2));
+
+        default:
+            syck_emit_scalar(e, "tag:yaml.org,2002:str", scalar_none, 0, 0, 0, VALUE, strlen(VALUE));
             break;
     }
         
@@ -34,13 +34,14 @@ void emitter_handler(SyckEmitter *e, st_data_t id)
 int main(int argc, char *argv[])
 {
     SyckEmitter *e;
+    int k;
 
     e = syck_new_emitter();
     syck_emitter_handler(e, emitter_handler);
     syck_output_handler(e, output_handler);
-    syck_emitter_mark_node(e, 1);
-    syck_emitter_mark_node(e, 2);
-    syck_emitter_mark_node(e, 3);
+    for (k = 1; k < LENGTH; k++) {
+        syck_emitter_mark_node(e, k);
+    }
     syck_emit(e, 1);
     syck_emitter_flush(e, 0);
     syck_free_emitter(e);
